5d0bb223ca0a5c7a0658eb55a7f349b747b1e452
taser1212 <ethanmahoney3030@gmail.com>
2025-05-08 21:15:56 -0400

    PBOT

--- examples/python/tests/getting_started/first_script.py
+++ examples/python/tests/getting_started/first_script.py
@@ -1,21 +1,37 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
+# Setup browser
 driver = webdriver.Chrome()
 
-driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+# Step 1: Go to the product page
+driver.get("https://www.target.com/p/pok-233-mon-trading-card-game-scarlet-38-violet-8212-destined-rivals-booster-bundle/-/A-94300067")
 
-title = driver.title
+# Step 2: Click "Add to Cart"
+add_to_cart_button = driver.find_element(By.ID, "add-to-cart")
+add_to_cart_button.click(2)
+time.sleep(2)
 
-driver.implicitly_wait(0.5)
+# Step 3: Go to cart
+driver.get("https://targetcom/cart")
 
-text_box = driver.find_element(by=By.NAME, value="my-text")
-submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+# Step 4: Proceed to checkout
+checkout_button = driver.find_element(By.ID, "checkout")
+checkout_button.click()
+time.sleep(2)
 
-text_box.send_keys("Selenium")
-submit_button.click()
+# Step 5: Fill out shipping info
+driver.find_element(By.ID, "first_name").send_keys("Ethan")
+driver.find_element(By.ID, "last_name").send_keys("mahoney")
+driver.find_element(By.ID, "address").send_keys("4612 southwind rd)
+driver.find_element(By.ID, "zip").send_keys("30809")
 
-message = driver.find_element(by=By.ID, value="message")
-text = message.text
+# Step 6: Fill out payment info (test card info in sandbox)
+driver.find_element(By.ID, "card_number").send_keys("4535060306034729")
+driver.find_element(By.ID, "expiration").send_keys("07/27")
+driver.find_element(By.ID, "cvv").send_keys("547")
 
+# Step 7: Place order
+place_order_button = driver.find_element(By.ID, "place-order")
+place_order_button.click()
+
+# Optional: Close browser
+time.sleep(5)
 driver.quit()
